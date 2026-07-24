a4558718cb8f7136ad718335cdbfde184a36b9d6
Trifall <JerrenT@Trifall.com>
2026-07-24 13:53:17 -0400

    fix: add delay to text box input for better linux compatibility

--- main/src/shortcuts/text-box.ts
+++ main/src/shortcuts/text-box.ts
@@ -14,39 +14,53 @@ const AUTO_CLEAR = [
   '/' // Command
 ]
 
-export function typeInChat (text: string, send: boolean, clipboard: HostClipboard) {
-  clipboard.restoreShortly((clipboard) => {
+export async function typeInChat (text: string, send: boolean, clipboard: HostClipboard) {
+  clipboard.restoreShortly(async (clipboard) => {
     const modifiers = process.platform === 'darwin' ? [Key.Meta] : [Key.Ctrl]
 
     if (text.startsWith(PLACEHOLDER_LAST)) {
       text = text.slice(`${PLACEHOLDER_LAST} `.length)
       clipboard.writeText(text)
+      await delay(10)
       uIOhook.keyTap(Key.Enter, modifiers)
     } else if (text.endsWith(PLACEHOLDER_LAST)) {
       text = text.slice(0, -PLACEHOLDER_LAST.length)
       clipboard.writeText(text)
+      await delay(10)
       uIOhook.keyTap(Key.Enter, modifiers)
+      await delay(10)
       uIOhook.keyTap(Key.Home)
+      await delay(10)
       // press twice to focus input when using controller
       uIOhook.keyTap(Key.Home)
+      await delay(10)
       uIOhook.keyTap(Key.Delete)
     } else {
       clipboard.writeText(text)
+      await delay(10)
       uIOhook.keyTap(Key.Enter)
       if (!AUTO_CLEAR.includes(text[0])) {
+        await delay(10)
         uIOhook.keyTap(Key.A, modifiers)
       }
     }
 
-    uIOhook.keyTap(Key.V, modifiers)
+    await delay(30)
 
+    uIOhook.keyTap(Key.V, modifiers)
+    await delay(10)
     if (send) {
       uIOhook.keyTap(Key.Enter)
+      await delay(10)
       // restore the last chat
       uIOhook.keyTap(Key.Enter)
+      await delay(10)
       uIOhook.keyTap(Key.ArrowUp)
+      await delay(10)
       uIOhook.keyTap(Key.ArrowUp)
+      await delay(10)
       uIOhook.keyTap(Key.Escape)
+      await delay(10)
     }
   })
 }
@@ -56,14 +70,14 @@ export function stashSearch (
   clipboard: HostClipboard,
   overlay: OverlayWindow
 ) {
-  clipboard.restoreShortly((clipboard) => {
+  clipboard.restoreShortly(async (clipboard) => {
     overlay.assertGameActive()
     clipboard.writeText(text)
 
     if (process.platform === 'linux') {
       return (async () => {
         // XWayland/Wine needs focus and non-zero-duration key transitions
-        await delay(10)
+        await delay(20)
 
         uIOhook.keyToggle(Key.Ctrl, 'down')
         try {
